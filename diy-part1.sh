5cf2024481ace0d149e6220c54f0f89347e383dd
Tok0ch1 <110771840+Tok0ch1@users.noreply.github.com>
2025-07-03 23:48:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git modem https://github.com/FUjr/modem_feeds.git;main' feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package\nsrc-git passwall https://github.com/xiaorouji/openwrt-passwall-packages\nsrc-git modem https://github.com/FUjr/modem_feeds.git;main' feeds.conf.default
 git clone https://github.com/Siriling/5G-Modem-Support package/luci-app-modem
 
